3907346703b6a7523a397832edfd2b202fc8345a
huthoinguyn <huthoi99@gmail.com>
2022-04-23 21:13:08 +0700

    up

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const cards = document.querySelectorAll(".card");
 const cart = document.querySelector(".cart");
 let index = 0;
-cards.forEach((item) => item.querySelector("button").addEventListener("click", handleaddProduct));
+cards.forEach((item) =>
+  item.querySelector("button").addEventListener("click", handleaddProduct)
+);
 
 // function handleQuantity() {}
 // var sl = handleQuantity(quantity);
 let toTal = 0;
-Number(toTal)
+Number(toTal);
 let number = 0;
-Number(number)
+Number(number);
 function handleaddProduct(event) {
   const card = event.target.parentNode.parentNode;
   const imagesrc = card.querySelector("img").src;
@@ -16,51 +18,49 @@ function handleaddProduct(event) {
   const price = card.querySelector(".price").innerText;
   let money = parseFloat(price.slice(1));
   index++;
-  addCart(index, imagesrc, name, money, toTal);
-  const cartItems = document.querySelectorAll(".cart-item");
-  [...cartItems].forEach(item => item.querySelector(".quantity").addEventListener("change",handleQuantity))
-  console.log("🚀 ~ file: main.js ~ line 20 ~ handleaddProduct ~ handleQuantity", handleQuantity(event))
-  function handleQuantity(event){
-    number = parseInt(event.target.value);
-    console.log("🚀 ~ file: main.js ~ line 21 ~ handleQuantity ~ number", number)
-    return number;
-  }
-  toTal = money * handleQuantity;
+  addCart(index, imagesrc, name, money);
+  // const cartItems = document.querySelectorAll(".cart-item");
+  // [...cartItems].forEach(item => item.querySelector(".quantity").addEventListener("change",handleQuantity))
+  // function handleQuantity(event){
+  //   number = parseInt(event.target.value);
+  //   console.log("🚀 ~ file: main.js ~ line 21 ~ handleQuantity ~ number", number)
+  //   return number;
+  // }
+  // toTal = money * handleQuantity;
 }
-//   function handleQuantity(quantity) {
-
-// }
 
 const table = document.querySelector("table");
 
-function addCart(index, imagesrc, name, money, toTal) {
+function addCart(index, imagesrc, name, money) {
   const templateCart = `
   <tr class="cart-item">
     <td>${index}</td>
     <td style="width:200px;text-align: center;"><img class="cart-item-img" src="${imagesrc}" alt=""></td>
-    <td>${name}</td>
-    <td>${money}</td>
+    <td >${name}</td>
+    <td class="product-price">$${money}</td>
     <td>
-    <select class="quantity">
-    <option value="0">0</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-    <option value="11">11</option>
-    <option value="12">12</option>
-  </select>
+    <input type="number" value="1" min="1" style="width:40%;" name="" id="">
     </td>
-    <td>${toTal}</td>
+    <td></td>
     </tr>
     `;
 
   table.insertAdjacentHTML("beforeend", templateCart);
   // var quantity = cartItem.querySelector(".quantity");
+  cartTotal();
+}
+
+function cartTotal() {
+  const cartItems = document.querySelectorAll(".cart-item");
+  let total = 0;
+  cartItems.forEach((item) => {
+    const productQuantity = item.querySelector("input").value;
+    const productPrice = parseFloat(
+      item.querySelector(".product-price").innerText.slice(1)
+    );
+    let productTotal = productPrice * productQuantity;
+    total += productTotal;
+  });
+  const intoMoney = document.querySelector(".cart-total");
+  intoMoney.querySelector("span").innerHTML = total;
 }
